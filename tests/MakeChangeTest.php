7edae687e561299eab3ad5497ffcb0790946f399
Calla Rudolph <callarudolph@gmail.com>
2017-07-05 19:04:40 -0700

    write test for dimes

--- tests/MakeChangeTest.php
+++ tests/MakeChangeTest.php
@@ -28,6 +28,19 @@
             //Assert
             $this->assertEquals(['quarters' => 0, 'dimes' => 0, 'nickels' => 1, 'pennies' => 3], $result);
         }
+
+        function testMakeChangeDimes()
+        {
+            //Arrange
+            $test_MakeChange = new MakeChange;
+            $input = '16';
+
+            //Act
+            $result = $test_MakeChange->convertCoin($input);
+
+            //Assert
+            $this->assertEquals(['quarters' => 0, 'dimes' => 1, 'nickels' => 1, 'pennies' => 1], $result);
+        }
     }
 
 ?>
