6a42880dd1bca1b0311737e61d08701b2efbfb1f
Calla Rudolph <callarudolph@gmail.com>
2017-07-05 19:09:56 -0700

    pass dimes test

--- src/MakeChange.php
+++ src/MakeChange.php
@@ -5,9 +5,11 @@
         {
             $coins_array = ['quarters' => 0, 'dimes' => 0, 'nickels' => 0, 'pennies' => 0];
 
-            $nickels = intval($input / 5);
-            $pennies = intval($input - ($nickels * 5));
+            $dimes = intval($input / 10);
+            $nickels = intval(($input - ($dimes * 10)) / 5);
+            $pennies = intval($input - ($dimes * 10) - ($nickels * 5));
 
+            $coins_array['dimes'] = $dimes;
             $coins_array['nickels'] = $nickels;
             $coins_array['pennies'] = $pennies;
 
