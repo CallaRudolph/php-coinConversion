a9e4d10ac52c33b421aaa90e2ac9eec2a569a0e6
Calla Rudolph <callarudolph@gmail.com>
2017-07-05 18:56:14 -0700

    correctly return penny input

--- src/MakeChange.php
+++ src/MakeChange.php
@@ -3,7 +3,13 @@
     {
         function convertCoin($input)
         {
-          
+          $coins_array = ['quarters' => 0, 'dimes' => 0, 'nickels' => 0, 'pennies' => 0];
+
+          $pennies = intval($input);
+
+          $coins_array['pennies'] = $pennies;
+
+          return $coins_array;
         }
     }
 ?>
