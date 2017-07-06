9c802464838e60cb5d23c373acaf780687c9a2fe
Calla Rudolph <callarudolph@gmail.com>
2017-07-05 19:13:33 -0700

    pass quarters test

--- src/MakeChange.php
+++ src/MakeChange.php
@@ -5,10 +5,12 @@
         {
             $coins_array = ['quarters' => 0, 'dimes' => 0, 'nickels' => 0, 'pennies' => 0];
 
-            $dimes = intval($input / 10);
-            $nickels = intval(($input - ($dimes * 10)) / 5);
-            $pennies = intval($input - ($dimes * 10) - ($nickels * 5));
+            $quarters = intval($input / 25);
+            $dimes = intval(($input - ($quarters * 25)) / 10);
+            $nickels = intval(($input - ($quarters * 25) - ($dimes * 10)) / 5);
+            $pennies = intval($input - ($quarters * 25) - ($dimes * 10) - ($nickels * 5));
 
+            $coins_array['quarters'] = $quarters;
             $coins_array['dimes'] = $dimes;
             $coins_array['nickels'] = $nickels;
             $coins_array['pennies'] = $pennies;
